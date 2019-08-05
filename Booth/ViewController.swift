3bdfc245f5eb3c6d078b2d7f7292c346956f2608
Fanglin Liu <fanglinliu@gmail.com>
2019-08-05 14:06:35 +0800

    Initial Commit

---
+++ Booth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Booth
+//
+//  Created by Fannie on 5/8/2019.
+//  Copyright © 2019 cmbu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
