d783f6f5e1580f7a5b78ea1de5d5c2f63ef0279d
Fanglin Liu <fanglinliu@gmail.com>
2019-08-05 14:55:14 +0800

    add main screen UI

--- Booth/ViewController.swift
+++ Booth/ViewController.swift
@@ -9,10 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet private weak var previewView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction private func takePhoto(_ takePhotoButton: UIButton) {
+        
+    }
+    
+    @IBAction private func viewPhotos(_ viewPhotosButton: UIButton) {
+        
     }
 
 
