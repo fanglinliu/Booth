16b71ef30d14f28770e1b24a4a0572ded985df9d
Fanglin Liu <fanglinliu@gmail.com>
2019-08-05 19:31:24 +0800

    add take picture and show photo

--- Booth/ViewController.swift
+++ Booth/ViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import MobileCoreServices
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet private weak var previewView: UIImageView!
+    private var captueMode = true;
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,11 +20,50 @@ class ViewController: UIViewController {
     }
     
     @IBAction private func takePhoto(_ takePhotoButton: UIButton) {
-        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            
+            let picker = UIImagePickerController()
+            
+            picker.sourceType = .camera
+            picker.delegate = self
+            self.present(picker, animated: true, completion: nil)
+        } else {
+            
+            let alertVc = UIAlertController(title: "提示", message: "相机不可用", preferredStyle: .alert)
+            let sureAction = UIAlertAction(title: "确定", style: .default, handler: nil)
+            alertVc.addAction(sureAction)
+            
+            self.present(alertVc, animated: true, completion: nil)
+        }
     }
     
     @IBAction private func viewPhotos(_ viewPhotosButton: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            
+            let picker = UIImagePickerController()
+            picker.sourceType = .photoLibrary
+            picker.delegate = self
+            
+            self.present(picker, animated: true, completion: nil)
+        }
+    }
+    
+    func isFromPhotoLibray(_ info: [UIImagePickerController.InfoKey : Any]) -> Bool {
+        return info[UIImagePickerController.InfoKey.phAsset] != nil;
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
+        previewView.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage;
+        picker.dismiss(animated: true, completion: nil)
+
+        if let image = previewView?.image, !isFromPhotoLibray(info) {
+            UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 
 
